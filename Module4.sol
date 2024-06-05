e1e5ac35ba19a907cecf45339e04925c0ad9c667
rhenellona <129043577+rhenellona@users.noreply.github.com>
2024-06-05 11:56:09 +0800

    Update Module4.sol

--- Module4.sol
+++ Module4.sol
@@ -5,23 +5,18 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
-    mapping(uint256 => uint256) public KinsClass;
-    mapping(uint256 => string) public KinsClassNames; 
-    mapping(address => uint256[]) private redeemedKinsClasses; 
+    struct KinsClass {
+        uint256 price;
+        string name;
+    }
+
+    mapping(uint256 => KinsClass) public kinsClasses;
+    mapping(address => uint256[]) private redeemedKinsClasses;
 
     event KinsClassRedeemed(address indexed redeemer, uint256 indexed kinId, uint256 price);
 
     constructor(uint256 initialSupply) ERC20("Degen", "DGN") Ownable(msg.sender) {
         _mint(msg.sender, initialSupply);
-
-        KinsClass[1] = 400;
-        KinsClassNames[1] = "Amethyst";
-        KinsClass[2] = 300;
-        KinsClassNames[2] = "Sapphire";
-        KinsClass[3] = 20;
-        KinsClassNames[3] = "Beryl";
-        KinsClass[4] = 450;
-        KinsClassNames[4] = "Garnet";
     }
 
     function checkDGNTokenBal() external view returns (uint256) {
@@ -44,36 +39,35 @@ contract DegenToken is ERC20, Ownable {
         return true;
     }
 
-    function redeemKinsClass(uint256 kinId) external {
-        uint256 kinPrice = KinsClass[kinId];
-        require(kinPrice > 0, "Kins Class does not exist");
-        require(balanceOf(msg.sender) >= kinPrice, "Insufficient balance");
+    function createKinsClass(uint256 kinId, uint256 price, string memory name) public onlyOwner {
+        require(kinsClasses[kinId].price == 0, "Kins Class already exists");
+        require(price > 0, "Price must be greater than zero");
+        kinsClasses[kinId] = KinsClass(price, name);
+    }
 
-        _transfer(msg.sender, address(this), kinPrice);
-        redeemedKinsClasses[msg.sender].push(kinId); 
-        emit KinsClassRedeemed(msg.sender, kinId, kinPrice);
+    function redeemKinsClass(uint256 kinId) external {
+        KinsClass memory kinsClass = kinsClasses[kinId];
+        require(bytes(kinsClass.name).length > 0, "Kins Class does not exist");
+        require(balanceOf(msg.sender) >= kinsClass.price, "Insufficient balance");
+
+        _transfer(msg.sender, address(this), kinsClass.price);
+        redeemedKinsClasses[msg.sender].push(kinId);
+        emit KinsClassRedeemed(msg.sender, kinId, kinsClass.price);
     }
 
     function balanceOf(address account) public view virtual override returns (uint256) {
         return super.balanceOf(account);
     }
 
-    function getKinsClassPriceandName(uint256 kinId) public view returns (uint256, string memory) {
-    require(KinsClass[kinId] > 0, "No existing KinID");
-    return (KinsClass[kinId], KinsClassNames[kinId]);
-    }
-
     function getRedeemedKins(address player) public view returns (uint256[] memory, string[] memory) {
-    uint256[] memory redeemedIds = redeemedKinsClasses[player];
-    string[] memory redeemedNames = new string[](redeemedIds.length);
+        uint256[] memory redeemedIds = redeemedKinsClasses[player];
+        string[] memory redeemedNames = new string[](redeemedIds.length);
 
-    for (uint256 i = 0; i < redeemedIds.length; i++) {
-        uint256 kinId = redeemedIds[i];
-        redeemedNames[i] = KinsClassNames[kinId];
+        for (uint256 i = 0; i < redeemedIds.length; i++) {
+            uint256 kinId = redeemedIds[i];
+            redeemedNames[i] = kinsClasses[kinId].name;
+        }
+
+        return (redeemedIds, redeemedNames);
     }
-
-    return (redeemedIds, redeemedNames);
-    }
-
-    }
-
+}
