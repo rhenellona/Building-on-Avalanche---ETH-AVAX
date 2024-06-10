75a22175e689715938cf54d4fe9b7a30c004f983
rhenellona <129043577+rhenellona@users.noreply.github.com>
2024-06-10 09:35:34 +0800

    Update Module4.sol

--- Module4.sol
+++ Module4.sol
@@ -2,9 +2,8 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract DegenToken is ERC20 {
     struct KinsClass {
         uint256 price;
         string name;
@@ -14,8 +13,9 @@ contract DegenToken is ERC20, Ownable {
     mapping(address => uint256[]) private redeemedKinsClasses;
 
     event KinsClassRedeemed(address indexed redeemer, uint256 indexed kinId, uint256 price);
+    event RedemptionSuccess(address indexed redeemer, uint256 indexed kinId, string message); // New event
 
-    constructor(uint256 initialSupply) ERC20("Degen", "DGN") Ownable(msg.sender) {
+    constructor(uint256 initialSupply) ERC20("Degen", "DGN") {
         _mint(msg.sender, initialSupply);
     }
 
@@ -23,7 +23,7 @@ contract DegenToken is ERC20, Ownable {
         return balanceOf(msg.sender);
     }
 
-    function mintToken(address to, uint256 amount) public onlyOwner {
+    function mintToken(address to, uint256 amount) public {
         require(amount >= 250, "Amount to be minted should be greater than or equal to 250");
         _mint(to, amount);
     }
@@ -33,26 +33,32 @@ contract DegenToken is ERC20, Ownable {
         _burn(msg.sender, amount);
     }
 
-    function transferToken(address recipient, uint256 amount) public virtual returns (bool) {
+    function transferToken(address recipient, uint256 amount) public returns (bool) {
         require(amount <= balanceOf(_msgSender()), "ERC20: transfer amount exceeds balance");
         _transfer(_msgSender(), recipient, amount);
         return true;
     }
 
-    function createKinsClass(uint256 kinId, uint256 price, string memory name) public onlyOwner {
+    function createKinsClass(uint256 kinId, uint256 price, string memory name) public {
         require(kinsClasses[kinId].price == 0, "Kins Class already exists");
         require(price > 0, "Price must be greater than zero");
         kinsClasses[kinId] = KinsClass(price, name);
     }
 
     function redeemKinsClass(uint256 kinId) external {
-        KinsClass memory kinsClass = kinsClasses[kinId];
-        require(bytes(kinsClass.name).length > 0, "Kins Class does not exist");
-        require(balanceOf(msg.sender) >= kinsClass.price, "Insufficient balance");
+        KinsClass storage kinsClass = kinsClasses[kinId]; 
+        require(bytes(kinsClass.name).length > 0, "Kins Class does not exist"); 
+        uint256 price = kinsClass.price; 
+        require(balanceOf(msg.sender) >= price, "You don't have enough balance"); 
 
-        _transfer(msg.sender, address(this), kinsClass.price);
-        redeemedKinsClasses[msg.sender].push(kinId);
-        emit KinsClassRedeemed(msg.sender, kinId, kinsClass.price);
+        uint256[] storage redeemedIds = redeemedKinsClasses[msg.sender];
+        for (uint256 i = 0; i < redeemedIds.length; i++) {
+            require(redeemedIds[i] != kinId, "You've already redeemed this.");
+        }
+
+        _transfer(msg.sender, address(this), price); 
+        redeemedKinsClasses[msg.sender].push(kinId); 
+        emit KinsClassRedeemed(msg.sender, kinId, price);
     }
 
     function getRedeemedKins(address player) public view returns (uint256[] memory, string[] memory) {
